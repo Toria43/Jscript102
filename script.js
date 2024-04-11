082f63a4786b6b21d4db3d8771dd21bda93562d5
Victory <ahereosamudiamen@gmail.com>
2024-04-11 15:14:19 +0100

    functions!

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ if (hasDriversLicense) console.log('I can Drive! :D')
 
 //const interface = 'Audio';
 //const private = 534;
+
+/*functions*/
+function logger() {
+    console.log('My name is Victory');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples,pineapples) {
+    console.log(apples, pineapples);
+    const juice = `Juice with ${apples} apples and ${pineapples} pineapples.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const applePineappleJuice = fruitProcessor(2, 4);
+console.log(applePineappleJuice);
