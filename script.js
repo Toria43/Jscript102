c4e63247fd0ceee3ee3af54351d356c39078df00
Victory <ahereosamudiamen@gmail.com>
2024-04-20 07:51:14 +0100

    object methods

--- script.js
+++ script.js
@@ -146,32 +146,109 @@ const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -
 console.log(ages);
 
 /*Methods(Basic Array Operators)*/
-const friends = ['Timmy', 'Joy', 'Tobi'];
-friends.push('Elizabeth');
-console.log(friends);
+const friend = ['Timmy', 'Joy', 'Tobi'];
+// Add Elements
+const newLength = friend.push('Elizabeth');
+console.log(friend);
+console.log (newLength);
+
+friend.unshift('Simi');
+console.log(friend);
+// Remove Elements
+friend.pop(); //Last
+const popped = friend.pop();
+console.log(popped);
+console.log(friend);
+
+friend.shift(); //First
+console.log(friend);
+
+console.log(friend.indexOf('Joy'));
+console.log(friend.indexOf('Simi'));
+
+friends.push(24);
+console.log(friend.includes('Joy'));
+console.log(friend.includes('Simi'));
+// this wont work because 24 to string is different from 24 to number and it would give a FALSE
+console.log(friend.includes('24'));
+
+if (friend.includes('Joy')) {
+    console.log('You have a friend called Joy!')
+}
 
 
+/*Challenge 2*/
+/* FROM CHALLENGE 5
+const bill = 100;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+console.log(` The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+*/
+const calcTip = function (bill) {
+    return  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+}
+// Alternatively, const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
 
 
+/*Objects*/
+// objects are used to group variables that belong together, and its order do not matter at all
+const victoryArray = [
+    'Victory',
+    'Agholor',
+    2040 - 2003,
+    'student',
+    ['Timmy', 'Tobi', 'Joy']
+];
+// object literal sentence
+const vicky = {
+    firstName: 'Victory',
+    lastName: 'Agholor',
+    age: 2037-2003,
+    job: 'teacher',
+    friends: ['Timmy', 'Tobi', 'Joy']
+};
+console.log(vicky);
 
+/*Dot Notation - can only use real property name*/
+console.log(vicky.lastName);
+/*Bracket Notation*/
+console.log(vicky['lastName']);
 
+const nameKey = 'Name';
+console.log(vicky['first' + nameKey]);
+console.log(vicky['last' + nameKey]);
 
+const interestedIn = prompt('What do you want to know about Vicky? Choose between firstName, lastName, age, job, friends');
+if (vicky[interestedIn]) {
+console.log(vicky[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, friends');
+}
+vicky.location = 'Nigeria';
+vicky['twitter'] = '@victoryagholor';
+console.log(vicky);
 
+console.log(`${vicky.firstName} has ${vicky.friends.length}, and her best friend is called ${vicky.friends[0]}`);
 
+/* Object Methods*/
+const vicky = {
+    firstName: 'Victory',
+    lastName: 'Agholor',
+    birthYear: 2003,
+    job: 'teacher',
+    friends: ['Timmy', 'Tobi', 'Joy'],
+    hasDriversLicense: true,
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    calAge: function (birthYear) {
+        return 2037 - birthYear;
+    }
+};
+console.log(vicky.calAge(1991));
+console.log(vicky['calAge'](1991))
 
 
 
