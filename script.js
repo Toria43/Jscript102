0f6ae7b9b5b416b133c5125b2b481de054efd44a
Victory <ahereosamudiamen@gmail.com>
2024-04-23 13:13:20 +0100

    Javascript Fundamentals 2 completed

--- script.js
+++ script.js
@@ -81,29 +81,29 @@ function fruitProcessor(apples,pineapples) {
 console.log(fruitProcessor(2, 3));
 
 /*Challenge 1*/
-const calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(calcAverage);
-//Value 1
-let scoreDolphins = calcAverage(44, 23, 71);
-let scoreKoalas = calcAverage(65, 54, 49);
-console.log(scoreDolphins, scoreKoalas);
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage);
+// //Value 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-    if (avgDolphins>= 2 * avgKoalas){
-        console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
-    } else if (avgKoalas>= 2 * avgKoalas) {
-        console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
-    } else {
-        console.log('No team wins...');
-    }
-}
-checkWinner(scoreDolphins, scoreKoalas);
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins>= 2 * avgKoalas){
+//         console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas>= 2 * avgKoalas) {
+//         console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//     } else {
+//         console.log('No team wins...');
+//     }
+// }
+// checkWinner(scoreDolphins, scoreKoalas);
 
-//Value 2
-scoreDolphins = calcAverage(85, 54, 41);
-scoreKoalas = calcAverage(23, 34, 27);
-console.log(scoreDolphins, scoreKoalas);
-checkWinner(scoreDolphins, scoreKoalas);
+// //Value 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
 
 /*Arrays*/
 const friend1 = 'Timmy';
@@ -183,15 +183,15 @@ const bill = 100;
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 console.log(` The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
-const calcTip = function (bill) {
-    return  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
-}
-// Alternatively, const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// const calcTip = function (bill) {
+//     return  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// }
+// // Alternatively, const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
 
 
 /*Objects*/
@@ -403,7 +403,35 @@ while (dice !== 6) {
 }
 
 
+/* CHALLENGE #4*/
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
 
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+  
+  for(let i = 0; i < bills.length; i++) {
+      const tip = calcTip(bills[i]);
+      tips.push(tip);
+      totals.push(tip + bills[i]);
+  }
+      console.log(bills);
+      console.log(tips);
+      console.log(totals);
+      
+  const calcAverage = function(arr) {
+      let sum = 0;
+      for(let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i]
+      }
+   return sum / arr.length   
+  }
+  
+  console.log(calcAverage(totals));
 
 
 
